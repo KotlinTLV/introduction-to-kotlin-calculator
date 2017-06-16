cb8087a12a4a99d0f43555526a2402e821030bef
dorsamet <dori.samet@gmail.com>
2017-06-16 16:53:11 +0300

    Refactored Expr class and when statement

--- src/Calculator.kt
+++ src/Calculator.kt
@@ -1,28 +1,15 @@
 import java.util.*
 
-
 interface Calculator {
-    sealed class Expr {
-        class Operand(val number: Int) : Expr()
-        class BinaryOperator(val priority: Int, val expression: (Int, Int) -> Int) : Expr() {
-            fun apply(constant1: Operand, constant2: Operand): Operand =
-                    Operand(expression.invoke(constant1.number, constant2.number))
-        }
-    }
+    fun calculate(expression: String): Int
 
-    class Impl {
+    class Impl:Calculator {
         private val operandStack: Stack<Expr.Operand> = Stack()
         private val operationStack: Stack<Expr.BinaryOperator> = Stack()
-        private val operandMap = mapOf(
-                "+" to Expr.BinaryOperator(1, { x, y -> x + y }),
-                "-" to Expr.BinaryOperator(2, { x, y -> x - y }),
-                "*" to Expr.BinaryOperator(3, { x, y -> x * y }),
-                "/" to Expr.BinaryOperator(4, { x, y -> x / y })
-        )
 
         private fun getOperatorForExpression(expression: String): Expr.BinaryOperator {
-            if (operandMap.containsKey(expression)) {
-                return operandMap[expression]!!
+            if (Expr.isExpressionOperator(expression)) {
+                return Expr.BinaryOperator(expression)
             }
             throw IllegalArgumentException("Not an operator!")
         }
@@ -39,30 +26,42 @@ interface Calculator {
         private fun shouldContinueLoop(operator: Expr.BinaryOperator) = !operationStack.empty() &&
                 operator.priority <= operationStack.peek().priority
 
-        fun calculate(expression: String): Int {
+        private fun translate(current: String) =
+            if (Expr.isExpressionOperator(current)) {
+                getOperatorForExpression(current)
+            } else {
+                Expr.Operand(current)
+            }
+
+        override fun calculate(expression: String): Int {
             var builder: StringBuilder = StringBuilder()
             for (i in 0 until expression.length) {
                 val current = expression[i].toString()
-                when (current) {
-                    in operandMap.keys -> {
+                if (current.isNullOrBlank()) continue
+                val translatedAsExpr = translate(current)
+                when (translatedAsExpr) {
+                     is Expr.BinaryOperator -> {
                         builder = updateStringBuilderIfNecessary(builder)
-                        val operator: Expr.BinaryOperator = getOperatorForExpression(current)
                         if (!operationStack.empty()) {
-                            while (shouldContinueLoop(operator)) {
-                                operandStack.push(operator.apply(operandStack.pop(), operandStack.pop()))
+                            while (shouldContinueLoop(translatedAsExpr)) {
+                                operandStack.push(operationStack.pop()
+                                        .apply(operandStack.pop(), operandStack.pop()))
                             }
                         }
+                        operationStack.push(translatedAsExpr)
                     }
 
-                    in "0".."9" -> {
-                        builder.append(current)
-                    }
-
-                    else -> {
-                        builder = updateStringBuilderIfNecessary(builder)
+                    is Expr.Operand -> {
+                        builder.append(translatedAsExpr.number)
                     }
                 }
             }
+            operandStack.push(Expr.Operand(builder.toInteger()))
+            if (operationStack.size == 1) {
+                // postfix += stack.pop().getSign();
+                operandStack.push(operationStack.pop().apply(operandStack.pop(), operandStack.pop()))
+            }
+
             return operandStack.pop().number
         }
     }
--- src/Expr.kt
+++ src/Expr.kt
@@ -1,5 +1,26 @@
-/**
- * Created by dor on 6/16/2017.
- */
-class Expr {
+sealed class Expr {
+    companion object {
+        fun isExpressionOperator(expression: String): Boolean {
+            return expression in operatorMap
+        }
+
+        private val operatorMap = mapOf(
+                "+" to Expr.BinaryOperator(1, { x, y -> x + y }),
+                "-" to Expr.BinaryOperator(2, { x, y -> x - y }),
+                "*" to Expr.BinaryOperator(3, { x, y -> x * y }),
+                "/" to Expr.BinaryOperator(4, { x, y -> x / y })
+        )
+    }
+
+
+    class Operand(val number: Int) : Expr() {
+        constructor(number: String) : this(Integer.parseInt(number))
+    }
+
+    class BinaryOperator(val priority: Int, val expression: (Int, Int) -> Int) : Expr() {
+        constructor(expression: String): this(operatorMap[expression]!!.priority, operatorMap[expression]!!.expression)
+
+        fun apply(constant1: Operand, constant2: Operand): Operand =
+                Operand(expression.invoke(constant1.number, constant2.number))
+    }
 }
